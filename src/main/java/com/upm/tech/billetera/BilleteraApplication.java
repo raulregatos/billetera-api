c89eb23d415b7971787a7e3a930391fd5df03254
raulregatos <raulregatos@gmail.com>
2026-07-14 02:26:48 +0200

    init: motor transaccional base y dependencias

---
+++ src/main/java/com/upm/tech/billetera/BilleteraApplication.java
@@ -0,0 +1,13 @@
+package com.upm.tech.billetera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleteraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upm/tech/billetera/BilleteraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upm.tech.billetera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleteraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
